4a3ae738258c636670599f2bf66deaae0c218bf5
Noe Osorio <osorionoe@gmail.com>
2024-05-04 14:19:55 -0600

    Update AI logic to save a better script

--- gpt.py
+++ gpt.py
@@ -20,7 +20,7 @@ def create_script(text: str):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Eres experta en entender e interpretar una idea y convertirla en un script que enganche a miles de personas. Mejora la claridad y legibilidad de este texto, corrige redundania, basandote en su contexto. Manten el miso tono e intencion del narrador. Despues, como output, crea un script cinematografico inspirador que sea un monologo o narracion donde pueda hablar entre 1 y 2 minutos maximo con el siguiente texto:"},
+            {"role": "system", "content": "Eres experta en entender e interpretar una idea y convertirla en un script que enganche a miles de personas. Mejora la claridad y legibilidad de este texto, corrige redundania, basandote en su contexto. Manten el miso tono e intencion del narrador. Despues, como output, crea un script cinematografico inspirador que sea un monologo o narracion donde pueda hablar minimo 2 minutos y maximo 3 con el siguiente texto:"},
             {"role": "user", "content": text}
         ]
     )
